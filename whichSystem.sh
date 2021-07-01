b41a31bb10c962466f7707901caf1a53cce3aa5d
xansx <73076414+xansx@users.noreply.github.com>
2021-07-01 22:31:26 +0200

    Update whichSystem.sh

--- whichSystem.sh
+++ whichSystem.sh
@@ -3,19 +3,20 @@
 if [ "$1" ]; then
         ip="$1"
 else
-        echo "[!] Error: Missing IP"
+        echo -e "\n[!] Error: Missing IP\nUsage: $0 ip\nExample: $0 10.10.10.10\n"                       
         exit 1
 fi
 
-if ttl=$(ping -c 1 "$ip" | grep -E "^64 bytes" | awk {'print $6'} | sed 's/ttl=//') && [ "$ttl" ]; then
-        if [ "$ttl" -gt 64 ] && [ "$ttl" -lt 129 ]; then
-                echo "[+] Host System is Windows"
-        elif [ "$ttl" -gt 0 ] && [ "$ttl" -lt 65 ]; then
-                echo "[+] Host System is Linux"
+
+if ttl=$(ping -c 1 "$ip" | grep -E "^64 bytes" | awk {'print $6'} | sed 's/ttl=//') && [ "$ttl" ]; then  
+        if [ "$ttl" -gt 64 ] && [ "$ttl" -lt 129 ]; then                                                 
+                echo -e "\n[+] Host System is Windows\n"                                                 
+        elif [ "$ttl" -gt 0 ] && [ "$ttl" -lt 65 ]; then                                                 
+                echo -e "\n[+] Host System is Linux\n"                                                   
         else
-                echo "[!] System unknow"
+                echo -e "\n[!] System unknow\n"
         fi
 else
-        echo "[!] Error: Host Down"
+        echo -e "\n[!] Error: Host Down\n"
         exit 2
 fi
